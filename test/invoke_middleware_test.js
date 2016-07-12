05f898f7519e9a7265a5b7f93b82dff91c08f326
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-12 15:08:36 +0900

    Version incremented to 1.2.6

---
+++ test/invoke_middleware_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for invokeMiddleware.
+ * Runs with mocha.
+ */
+'use strict'
+
+const invokeMiddleware = require('../lib/middlewares/invoke_middleware.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('invoke-middleware', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Invoke middleware', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
