7fd458dbd7397b2ab1ad988d8e09c7b459a3cdbd
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-03 14:07:28 +0900

    Refactoring

---
+++ test/module_normalize_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for moduleNormalize.
+ * Runs with mocha.
+ */
+'use strict'
+
+const moduleNormalize = require('../lib/helper/module_normalize.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('module-normalize', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Module normalize', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
