28ad9ed6c573d90d78bad4190486f7d6aca06ced
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-03 21:52:50 +0900

    Refactoring

---
+++ test/endpoint_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for endpoint.
+ * Runs with mocha.
+ */
+'use strict'
+
+const endpoint = require('../lib/endpoint.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('endpoint', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Endpoint', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
